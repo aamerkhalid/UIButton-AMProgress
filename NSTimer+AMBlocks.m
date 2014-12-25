d4a77493680aa1c39d27c9dbfee40e40f86f110a
hassan.islam@swenggco-software.com <hassan.islam@swenggco-software.com>
2014-12-25 17:08:23 +0500

    Add Updated Files

---
+++ NSTimer+AMBlocks.m
@@ -0,0 +1,34 @@
+//
+//  NSTimer+AMBlocks.m
+//
+//  Created by Aamer Khalid on 25/12/2014.
+//
+
+#import "NSTimer+AMBlocks.h"
+
+@implementation NSTimer (AMBlocks)
++(id)scheduledTimerWithTimeInterval:(NSTimeInterval)inTimeInterval block:(void (^)())inBlock repeats:(BOOL)inRepeats
+{
+    void (^block)() = [inBlock copy];
+    id ret = [self scheduledTimerWithTimeInterval:inTimeInterval target:self selector:@selector(jdExecuteSimpleBlock:) userInfo:block repeats:inRepeats];
+    return ret;
+}
+
++(id)timerWithTimeInterval:(NSTimeInterval)inTimeInterval block:(void (^)())inBlock repeats:(BOOL)inRepeats
+{
+    void (^block)() = [inBlock copy];
+    id ret = [self timerWithTimeInterval:inTimeInterval target:self selector:@selector(jdExecuteSimpleBlock:) userInfo:block repeats:inRepeats];
+    return ret;
+}
+
++(void)jdExecuteSimpleBlock:(NSTimer *)inTimer;
+{
+    if([inTimer userInfo])
+    {
+        void (^block)() = (void (^)())[inTimer userInfo];
+        block();
+    }
+}
+
+
+@end
